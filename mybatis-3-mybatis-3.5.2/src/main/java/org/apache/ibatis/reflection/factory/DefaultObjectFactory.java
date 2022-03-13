d65c6b872bef555669f450d57b802d598f29d28a
Wen Zhao (Joan) <withzhaowen@126.com>
2022-03-13 09:14:18 +0800

    update reflect.factory 注释

--- mybatis-3-mybatis-3.5.2/src/main/java/org/apache/ibatis/reflection/factory/DefaultObjectFactory.java
+++ mybatis-3-mybatis-3.5.2/src/main/java/org/apache/ibatis/reflection/factory/DefaultObjectFactory.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2019 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2019 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.reflection.factory;
 
@@ -35,16 +35,23 @@ import org.apache.ibatis.reflection.Reflector;
 
 /**
  * @author Clinton Begin
+ * <p>
+ * 继承了 {@link ObjectFactory} 接口，是默认的对象工厂实现
+ * <p>
+ * 作为工厂，DefaultObjectFactory的 create方法用来生产对象，
+ * 而两个 create方法最终都用到了 instantiateClass方法
  */
 public class DefaultObjectFactory implements ObjectFactory, Serializable {
 
   private static final long serialVersionUID = -8855120656740914948L;
 
+  // 生产对象
   @Override
   public <T> T create(Class<T> type) {
     return create(type, null, null);
   }
 
+  // 生产对象
   @SuppressWarnings("unchecked")
   @Override
   public <T> T create(Class<T> type, List<Class<?>> constructorArgTypes, List<Object> constructorArgs) {
@@ -53,14 +60,29 @@ public class DefaultObjectFactory implements ObjectFactory, Serializable {
     return (T) instantiateClass(classToCreate, constructorArgTypes, constructorArgs);
   }
 
-  private  <T> T instantiateClass(Class<T> type, List<Class<?>> constructorArgTypes, List<Object> constructorArgs) {
+  /**
+   * 创建类的实例
+   * 能够通过反射找到与参数匹配的构造方法，然后基于反射调用改构造方法生成一个对象
+   *
+   * @param type                要创建实例的类
+   * @param constructorArgTypes 构造方法输入参数类型
+   * @param constructorArgs     构造方法输入参数
+   * @param <T>                 实例类型
+   * @return 创建的实例
+   */
+  private <T> T instantiateClass(Class<T> type, List<Class<?>> constructorArgTypes, List<Object> constructorArgs) {
     try {
+      // 初始化构造方法
       Constructor<T> constructor;
+      // 检查参数类型列表为 null 或者参数列表为 null
       if (constructorArgTypes == null || constructorArgs == null) {
+        // 如果为 null ，就获取无参构造函数
         constructor = type.getDeclaredConstructor();
         try {
+          // 适用无参构造函数创建对象
           return constructor.newInstance();
         } catch (IllegalAccessException e) {
+          // 如果发生异常，则修改构造函数的访问属性后再次尝试
           if (Reflector.canControlMemberAccessible()) {
             constructor.setAccessible(true);
             return constructor.newInstance();
@@ -69,11 +91,14 @@ public class DefaultObjectFactory implements ObjectFactory, Serializable {
           }
         }
       }
+      // 根据输入参数类型查找对应的构造器
       constructor = type.getDeclaredConstructor(constructorArgTypes.toArray(new Class[constructorArgTypes.size()]));
       try {
+        // 采用有参构造函数创建实例
         return constructor.newInstance(constructorArgs.toArray(new Object[constructorArgs.size()]));
       } catch (IllegalAccessException e) {
         if (Reflector.canControlMemberAccessible()) {
+          // 如果发生异常，则修改构造函数的访问属性后再次尝试
           constructor.setAccessible(true);
           return constructor.newInstance(constructorArgs.toArray(new Object[constructorArgs.size()]));
         } else {
@@ -81,14 +106,23 @@ public class DefaultObjectFactory implements ObjectFactory, Serializable {
         }
       }
     } catch (Exception e) {
+      // 收集所有的参数类型
       String argTypes = Optional.ofNullable(constructorArgTypes).orElseGet(Collections::emptyList)
-          .stream().map(Class::getSimpleName).collect(Collectors.joining(","));
+        .stream().map(Class::getSimpleName).collect(Collectors.joining(","));
+      // 收集所有的参数
       String argValues = Optional.ofNullable(constructorArgs).orElseGet(Collections::emptyList)
-          .stream().map(String::valueOf).collect(Collectors.joining(","));
+        .stream().map(String::valueOf).collect(Collectors.joining(","));
       throw new ReflectionException("Error instantiating " + type + " with invalid types (" + argTypes + ") or values (" + argValues + "). Cause: " + e, e);
     }
   }
 
+  /**
+   * 当传入的目标类型是一个接口时，改方法可以给出一个符合该接口的实现。
+   *
+   * 例如：当要创建一个 Map 对象时，最终会创建一个 HashMap 对象
+   * @param type
+   * @return
+   */
   protected Class<?> resolveInterface(Class<?> type) {
     Class<?> classToCreate;
     if (type == List.class || type == Collection.class || type == Iterable.class) {
--- mybatis-3-mybatis-3.5.2/src/main/java/org/apache/ibatis/reflection/factory/ObjectFactory.java
+++ mybatis-3-mybatis-3.5.2/src/main/java/org/apache/ibatis/reflection/factory/ObjectFactory.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2019 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2019 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.reflection.factory;
 
@@ -22,10 +22,13 @@ import java.util.Properties;
  * MyBatis uses an ObjectFactory to create all needed new Objects.
  *
  * @author Clinton Begin
+ *
+ * 基于反射来生产出各种对象
  */
 public interface ObjectFactory {
 
   /**
+   * 设置工厂的属性
    * Sets configuration properties.
    * @param properties configuration properties
    */
@@ -34,6 +37,8 @@ public interface ObjectFactory {
   }
 
   /**
+   * 传入一个类型，采用午餐构造方法生成这个类型的实例
+   *
    * Creates a new object with default constructor.
    * @param type Object type
    * @return
@@ -41,6 +46,8 @@ public interface ObjectFactory {
   <T> T create(Class<T> type);
 
   /**
+   * 传入一个目标类型、一个参数类型列表、一个参数值列表，根据参数列表找到相应的含参构造方法生成这个类型的实例
+   *
    * Creates a new object with the specified constructor and params.
    * @param type Object type
    * @param constructorArgTypes Constructor argument types
@@ -50,6 +57,8 @@ public interface ObjectFactory {
   <T> T create(Class<T> type, List<Class<?>> constructorArgTypes, List<Object> constructorArgs);
 
   /**
+   * 判断传入的类型是否是集合类
+   *
    * Returns true if this object can have a set of other objects.
    * It's main purpose is to support non-java.util.Collection objects like Scala collections.
    *
